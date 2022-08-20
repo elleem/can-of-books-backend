f19a70b2bf55fac4c5c02dde6cb84fa657e66951
Lauren Main <lauren.main28@gmail.com>
2022-08-20 15:35:36 -0700

    adds models

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const getBooks = require('./modules/handlers');
 
 const app = express();
 app.use(cors());
@@ -15,13 +16,19 @@ mongoose.connect(process.env.MONGO_CONNECTION);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('Mongoose is connected')
+  console.log('Mongoose is connected');
 });
 
 app.get('/test', (request, response) => {
 
-  response.send('test request received')
+  response.send('test request received');
 
-})
+});
+
+app.get('/books', getBooks);
+
+app.use((error, request, response, next) => {
+  response.status(500).send(`Error occurred in the server! ${error.message}`);
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
