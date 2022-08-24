f1d12b121f84c18f7c24f48bb49cd0a85eda9159
Lauren Main <lauren.main28@gmail.com>
2022-08-24 04:31:07 -0700

    adds backend post function and clear function

--- modules/handlers.js
+++ modules/handlers.js
@@ -2,7 +2,9 @@
 
 const Book = require('../models/books');
 
-const getBooks = async (request, response, next) => {
+const Handler = {}; 
+
+Handler.getBooks = async (request, response, next) => {
   try {
     // is I pass in a empty object, that tells Mongoose to get ALL the documents from the database
     const book = await Book.find({});
@@ -16,4 +18,20 @@ const getBooks = async (request, response, next) => {
   }
 };
 
-module.exports = getBooks;
+
+
+Handler.createBook = async (request, response, next) => {
+  try {
+    const book = await Book.create(request.body);
+    response.status(201).send(book);
+    // const noData = require('./nodata.js'); 
+    // response.send(noData);
+
+  } catch (error) {
+    error.customMessage = 'Something went wrong with adding your book: '; 
+    console.error(error.customMessage + error);
+    next(error);
+  }
+};
+
+module.exports = Handler;
